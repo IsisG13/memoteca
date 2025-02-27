a2534874e5f592d8a0b9cef66379e3d585907fc0
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-27 15:55:45 -0300

    estrutura basica pronta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './component/cabecalho/cabecalho.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MainComponent } from './component/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
